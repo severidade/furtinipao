c525bbbbffe0c7aafbe389745755c233201a9f44
severo <oliveira.severo@gmail.com>
2024-12-29 21:22:43 -0300

    feat: :rocket: Cria a seçao hero_fase

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ function App() {
       <div className="image-container-single">
         <ScrollZoomImage />
       </div>
-      <h1 className="app_title">O pão é de verdade e o café de coração</h1>
-      <p className="app_title_subtitle">
-        Arte, gastronomia e conforto esperam por você aqui.
-      </p>
+      <div className="hero_fase">
+        <h1 className="app_title">O pão é de verdade e o café de coração</h1>
+        <p className="app_title_subtitle">
+          Arte, gastronomia e conforto esperam por você aqui.
+        </p>
+      </div>
       <CallUber />
       <div className="tijolo">P</div>
       <div className="tijolo">P</div>

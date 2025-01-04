51499522aa3f3053301fb12284f823ca48f43b70
severo <oliveira.severo@gmail.com>
2025-01-04 19:28:02 -0300

    feat: :rocket: Cria Highlight componente

---
+++ src/components/HighlightGallery/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import styles from './HighlightGallery.module.css';
+
+export default function HighlightGallery() {
+  return (
+    <div
+      className={styles.container_highlight}
+    >
+      <h1>
+        Frutini padaria e confeitaria
+      </h1>
+    </div>
+  );
+}

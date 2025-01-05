5c75efcbe08d3dc3543f33205a504af4394317c7
severo <oliveira.severo@gmail.com>
2025-01-05 11:09:01 -0300

    feat: :rocket: Configura vite para permitir conexoes externas
    permite ver a página na rede interna

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Permite conexões externas
+    port: 5174, // Porta desejada (pode ser a mesma ou outra)
+  },
+});

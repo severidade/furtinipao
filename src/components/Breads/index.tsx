b1af1981cee9e462ead3e209efaa471b7943d4a8
severo <oliveira.severo@gmail.com>
2025-01-07 01:36:34 -0300

    feat: :lipstick: Fromatando galeria slider paes

--- src/components/Breads/index.tsx
+++ src/components/Breads/index.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable max-len */
 /* eslint-disable react/react-in-jsx-scope */
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 import BreadOrderButton from '../BreadOrderButton/index.tsx';
 import styles from './Breads.module.css';
 import { BreadsGalleryDataType } from '../../types/BreadsGalleryDataType.tsx';
@@ -11,6 +15,16 @@ type BreadsProps = {
 };
 
 export default function Breads({ DataSlider, DataSection } : BreadsProps) {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2.5, // Exibe 2 e meio
+    slidesToScroll: 2,
+    arrows: false,
+    centerMode: false,
+  };
+
   return (
     <div className={styles.container_breads}>
       <section className={styles.header_breads}>
@@ -18,7 +32,21 @@ export default function Breads({ DataSlider, DataSection } : BreadsProps) {
         <p className={styles.header_breads_subtitle}>{ DataSection[0].headerBreads.breadsSubtitle }</p>
       </section>
       <section className={styles.slider_breads}>
-        <p>{ DataSlider[1].breadName }</p>
+        <Slider {...settings}>
+          {DataSlider.map((i) => (
+            <>
+              <figure key={i.id} className={styles.slider}>
+                <img
+                  src={i.image}
+                  alt={i.altText}
+                  className={styles.highlight_image}
+                />
+              </figure>
+              <h3 className={styles.slider_title}>{i.breadName}</h3>
+            </>
+          ))}
+        </Slider>
+        {/* <p>{ DataSlider[1].breadName }</p> */}
       </section>
       <section className={styles.info_breads}>
         <p>{ DataSection[0].infoBreads }</p>

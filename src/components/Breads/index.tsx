bacea709aca4e1c32a853e96362696f3579eb64b
severo <oliveira.severo@gmail.com>
2025-01-07 10:23:50 -0300

    feat: :lipstick: Css slider pães

--- src/components/Breads/index.tsx
+++ src/components/Breads/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable max-len */
 /* eslint-disable react/react-in-jsx-scope */
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import BreadOrderButton from '../BreadOrderButton/index.tsx';
-import styles from './Breads.module.css';
+// import styles from './Breads.module.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Breads.css';
 import { BreadsGalleryDataType } from '../../types/BreadsGalleryDataType.tsx';
 import { BreadsSectionDataType } from '../../types/BreadsSectionDataType.tsx';
 
@@ -26,29 +27,29 @@ export default function Breads({ DataSlider, DataSection } : BreadsProps) {
   };
 
   return (
-    <div className={styles.container_breads}>
-      <section className={styles.header_breads}>
-        <h2 className={styles.header_breads_title}>{ DataSection[0].headerBreads.breadsTitle }</h2>
-        <p className={styles.header_breads_subtitle}>{ DataSection[0].headerBreads.breadsSubtitle }</p>
+    <div className="container_breads">
+      <section className="header_breads">
+        <h2 className="header_breads_title">{ DataSection[0].headerBreads.breadsTitle }</h2>
+        <p className="header_breads_subtitle">{ DataSection[0].headerBreads.breadsSubtitle }</p>
       </section>
-      <section className={styles.slider_breads}>
+      <section className="slider_breads">
         <Slider {...settings}>
           {DataSlider.map((i) => (
-            <>
-              <figure key={i.id} className={styles.slider}>
+            <div key={i.id}>
+              <figure className="slider">
                 <img
                   src={i.image}
                   alt={i.altText}
-                  className={styles.highlight_image}
+                  className="bread_highlight_image"
                 />
               </figure>
-              <h3 className={styles.slider_title}>{i.breadName}</h3>
-            </>
+              <h3 className="slider_title">{i.breadName}</h3>
+            </div>
           ))}
         </Slider>
-        {/* <p>{ DataSlider[1].breadName }</p> */}
+
       </section>
-      <section className={styles.info_breads}>
+      <section className="info_breads">
         <p>{ DataSection[0].infoBreads }</p>
       </section>
       <BreadOrderButton />

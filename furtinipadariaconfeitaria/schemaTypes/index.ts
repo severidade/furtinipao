f62b1353b0d19873bc29bc5b64b9a105d7705c84
severo <oliveira.severo@gmail.com>
2025-01-25 07:49:18 -0300

    feat: :rocket: Implementa Breads

--- furtinipadariaconfeitaria/schemaTypes/index.ts
+++ furtinipadariaconfeitaria/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import blockContent from './blockContent'
 import lunch from './lunch'
+import breads from './breads'
 
-export const schemaTypes = [blockContent, lunch]
+export const schemaTypes = [blockContent, lunch, breads]

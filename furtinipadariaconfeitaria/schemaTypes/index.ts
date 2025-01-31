6b9029af80a2442b4dd8ae99f72d719b0a1d4011
severo <oliveira.severo@gmail.com>
2025-01-31 16:14:11 -0300

    feat: :rocket: Schema para a seção endereço

--- furtinipadariaconfeitaria/schemaTypes/index.ts
+++ furtinipadariaconfeitaria/schemaTypes/index.ts
@@ -1,9 +1,10 @@
 import blockContent from './blockContent'
 import lunch from './lunch'
 import breads from './breads'
+import sectionAddress from "./sectionAddress";
 import history from './history'
 import events from './events'
 import whatsAppButton from './whatsAppButton'
 import highlightGallery from './highlightGallery'
 
-export const schemaTypes = [highlightGallery, blockContent, lunch, breads, history, events, whatsAppButton]
+export const schemaTypes = [highlightGallery, blockContent, lunch, breads, sectionAddress, history, events, whatsAppButton]
---
+++ furtinipadariaconfeitaria/schemaTypes/sectionAddress.js
@@ -0,0 +1,84 @@
+import { defineType } from 'sanity';
+
+export default defineType({
+  name: 'sectionAddress',
+  title: 'Seção - Endereço',
+  type: 'document',
+  fields: [
+    {
+      name: 'header',
+      title: 'Cabeçalho',
+      type: 'object',
+      fields: [
+        {
+          name: 'title',
+          title: 'Título',
+          type: 'string',
+        },
+        {
+          name: 'subtitle',
+          title: 'Subtítulo',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'content',
+      title: 'Detalhes adicionais',
+      type: 'blockContent',
+    },
+    {
+      name: 'gallerySlider',
+      title: 'Galeria de fotos',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'image',
+              title: 'Imagem',
+              type: 'image',
+              options: {
+                hotspot: true,
+              },
+              fields: [
+                {
+                  name: 'altText',
+                  title: 'Texto Alternativo - Descreve imagem para SEO',
+                  type: 'string',
+                },
+              ],
+            },
+          ],
+          preview: {
+            select: {
+              title: 'image.altText', // Pegando o texto alternativo da imagem
+              media: 'image',
+            },
+            prepare(selection) {
+              const { title, media } = selection;
+              return {
+                title: title || 'Sem descrição',
+                media: media,
+              };
+            },
+          },
+        },
+      ],
+    }
+  ],
+  preview: {
+    select: {
+      title: 'header.title',
+      subtitle: 'header.subtitle',
+    },
+    prepare(selection) {
+      const { title, media } = selection;
+      return {
+        title: title || 'Sem descrição',
+        media: media,
+      };
+    },
+  },
+});

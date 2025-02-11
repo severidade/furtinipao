d60ba2d153be658599f1b7fa9ac49a9b1fd377d4
severo <oliveira.severo@gmail.com>
2025-02-11 09:20:29 -0300

    feat: :rocket: novo app para teste do react query

---
+++ src/AppWithReactQuery.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable max-len */
+import './CSS/Halogenfonts.css';
+import './CSS/Macklinfonts.css';
+import './App.css';
+
+import Heder from './components/Heder/index.tsx';
+import {
+  SectionHeroWelcome, SectionHighlightGallery, SectionLunch, SectionBreads, SectionAddress, SectionOpeningHours, SectionTemplate,
+} from './sections/index.ts';
+import FixedWhatsAppButton from './components/FixedWhatsAppButton/index.tsx';
+import Footer from './components/Footer/index.tsx';
+import OrientationDetectorDevice from './components/OrientationDetectorDevice/index.tsx';
+
+function App() {
+  return (
+    <>
+      <Heder />
+      <SectionHeroWelcome id="sectionHeroWelcome" />
+      <SectionHighlightGallery id="highlightGallery" />
+      <SectionLunch id="lunch" />
+      <SectionBreads id="breads" />
+      <SectionAddress id="sectionAddress" />
+      <SectionOpeningHours id="openingHours" />
+      <SectionTemplate id="history" />
+      <SectionTemplate id="events" />
+      <FixedWhatsAppButton />
+      <Footer />
+      <OrientationDetectorDevice />
+    </>
+  );
+}
+
+export default App;
+
+// https://squoosh.app/ comprimir imagens
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './CSS/reset.css';
 import './index.css';
-import App from './App.tsx';
+// import App from './App.tsx';
+import AppWithReactQuery from './AppWithReactQuery.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppWithReactQuery />
   </StrictMode>,
 );

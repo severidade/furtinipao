8b29552a72ba8f7f4fc032c546f34bbc4473ba4c
severo <oliveira.severo@gmail.com>
2025-02-12 08:34:56 -0300

    feat: :rocket: Modifica pra usar AppWithReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './CSS/reset.css';
@@ -6,8 +7,12 @@ import './index.css';
 // import App from './App.tsx';
 import AppWithReactQuery from './AppWithReactQuery.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppWithReactQuery />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <AppWithReactQuery />
+    </StrictMode>
+  </QueryClientProvider>,
 );

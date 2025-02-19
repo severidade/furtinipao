e56a51706a2c56734245ec8a33397b0202a3d2a2
severo <oliveira.severo@gmail.com>
2025-02-19 10:34:53 -0300

    feat: :rocket: instala o google analitics

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable max-len */
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import './CSS/Halogenfonts.css';
 import './CSS/Macklinfonts.css';
 import './App.css';
 
-import Heder from './components/Heder/index.tsx';
+import Header from './components/Heder/index.tsx';
 import {
-  SectionHeroWelcome, SectionHighlightGallery, SectionLunch, SectionBreads, SectionAddress, SectionOpeningHours, SectionTemplate,
+  SectionHeroWelcome,
+  SectionHighlightGallery,
+  SectionLunch,
+  SectionBreads,
+  SectionAddress,
+  SectionOpeningHours,
+  SectionTemplate,
 } from './sections/index.ts';
 import FixedWhatsAppButton from './components/FixedWhatsAppButton/index.tsx';
 import Footer from './components/Footer/index.tsx';
 import OrientationDetectorDevice from './components/OrientationDetectorDevice/index.tsx';
 
+// Substitua pelo seu ID do Google Analytics (Formato: G-XXXXXXXXXX)
+const GA_ID = 'G-3FF6SN0BX5';
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(GA_ID);
+    ReactGA.send('pageview'); // Registra a visualização da página
+  }, []);
+
   return (
     <>
-      <Heder />
+      <Header />
       <SectionHeroWelcome id="sectionHeroWelcome" />
       <SectionHighlightGallery id="highlightGallery" />
       <SectionLunch id="lunch" />

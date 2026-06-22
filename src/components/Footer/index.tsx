2c06bd57ca42b189b3dec0496204be357dd34f85
severo <oliveira.severo@gmail.com>
2026-06-22 14:17:11 -0300

    feat: :bug: Corrgir o texto. no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -88,7 +88,7 @@ export default function Footer() {
 
         <div className={styles.opening_hours_footer}>
           <div className={styles.opening_hours_footer_info}>Terca a Sexta - 10 às 18h</div>
-          <div className={styles.opening_hours_footer_info}>Sábado - Aberto pra cursos e oficinas</div>
+          <div className={styles.opening_hours_footer_info}>Sábado - Aberto para cursos e oficinas</div>
           {/* <div className={styles.opening_hours_footer_info}>Sábado - 10 às 14h</div>
           <div className={styles.opening_hours_footer_info}>Domingo - 9 às 14h</div> */}
           {/* <div className={styles.opening_hours_footer_info}>Feriados - 9 às 20h</div> */}
